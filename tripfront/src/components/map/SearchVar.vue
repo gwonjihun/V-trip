062aa43f6f7574732acf718eadf15570363ac0f2
권지훈 <aspire51@naver.com>
2023-05-18 17:55:29 +0900

    search view create

---
+++ tripfront/src/components/map/SearchVar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-form>
+      <b-form-select>
+        <b-form-select-option-group>
+          <b-form-select-option>1</b-form-select-option>
+        </b-form-select-option-group>
+      </b-form-select>
+      <b-form-select>
+        <b-form-select-option-group>
+          <b-form-select-option>1</b-form-select-option>
+        </b-form-select-option-group>
+      </b-form-select>
+      <b-form-input>
+
+      </b-form-input>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
